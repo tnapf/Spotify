39144e8f246355c1617722da3523dee22b1b90cf
Robert Snedeker <bmxmikey2@gmail.com>
2023-05-24 10:54:48 -0400

    Created IncludedGroups constants

---
+++ src/Abstractions/Artist/IncludeGroups.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tnapf\Spotify\Abstractions\Artist;
+
+class IncludeGroups
+{
+    public const ALBUM = 'album';
+    public const SINGLE = 'single';
+    public const APPEARS_ON = 'appears_on';
+    public const COMPILATION = 'compilation';
+}
