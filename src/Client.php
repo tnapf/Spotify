f8b8514f3512d61e3701bff55c7eea916bfb2192
Robert Snedeker <bmxmikey2@gmail.com>
2023-05-24 09:01:03 -0400

    Created lazy loading

--- src/Client.php
+++ src/Client.php
@@ -3,16 +3,45 @@
 namespace Tnapf\Spotify;
 
 use Tnapf\Spotify\Rest\Albums;
+use Tnapf\Spotify\Rest\Artists;
 use Tnapf\Spotify\Rest\Tracks;
 
+/**
+ * @property-read Http $http
+ * @property-read Albums $albums
+ * @property-read Tracks $tracks
+ * @property-read Artists $artists
+ */
 class Client
 {
-    public readonly Tracks $tracks;
-    public readonly Albums $albums;
+    private array $rest = [];
 
     public function __construct(public readonly Http $http)
     {
-        $this->tracks = new Tracks($this->http);
-        $this->albums = new Albums($this->http);
+    }
+
+    public function __get(string $name): mixed
+    {
+        $public = ['http'];
+
+        if (in_array($name, $public)) {
+            return $this->$name;
+        }
+
+        $rest = [
+            'albums' => Albums::class,
+            'tracks' => Tracks::class,
+            'artists' => Artists::class,
+        ];
+
+        if (!isset($rest[$name])) {
+            return null;
+        }
+
+        if (!isset($this->rest[$name])) {
+            $this->rest[$name] = new $rest[$name]($this->http);
+        }
+
+        return $this->rest[$name];
     }
 }
