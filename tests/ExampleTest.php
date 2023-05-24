5370b575553af6e6d9e41c875a41a36c137ae27c
Robert Snedeker <bmxmikey2@gmail.com>
2023-05-23 21:36:56 -0400

    Created placeholder test for the moment

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,14 +1,13 @@
 <?php
 
-namespace Tnapf\Package\Test;
+namespace Tnapf\Spotify\Tests;
 
 use PHPUnit\Framework\TestCase;
-use Tnapf\Package\ExampleClass;
 
 class ExampleTest extends TestCase
 {
     public function testExample()
     {
-        $this->assertTrue((new ExampleClass())->returnTrue());
+        $this->assertTrue(true);
     }
 }
